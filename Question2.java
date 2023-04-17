7c8938b7cbdd1dfd4026c1e25add422bb8cec4a3
fate-wq <limbryan12@gmail.com>
2023-04-17 18:27:57 +0800

    question 2

--- Question2.java
+++ Question2.java
@@ -11,11 +11,11 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.print("Enter your height in meters: ");
+    //System.out.print("Enter your height in meters: ");
     double height = in.nextDouble();
-    System.out.print("Enter your weight in kilograms: ");
+   // System.out.print("Enter your weight in kilograms: ");
     double weight = in.nextDouble();
     double bmi = weight / (height * height);
-    System.out.println("Your BMI is " + bmi);
+    System.out.println(bmi);
   }
 }
