06574f0ca4bbef1a2b0a22b3eb580d0a7161b505
fate-wq <limbryan12@gmail.com>
2023-04-23 17:16:00 +0800

    question 4 retry

--- Question4.java
+++ Question4.java
@@ -17,10 +17,10 @@ public class Question4
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
      
-    Scanner in = new Scanner(System.in);
-        int base = in.nextInt();
+    Scanner input = new Scanner(System.in);
+        int base = input.nextInt();
         
-        for (int i = 1; i <= base; i++) {
+        for (int i = base; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
